3ba0726898efc6aa39a3d70387a3b9d650c4d1bd
JairoDuenas <jairoduenas.ing@gmail.com>
2022-07-11 08:44:45 -0500

    Subiendo archivos

---
+++ src/components/Home.vue
@@ -0,0 +1,16 @@
+<!-- eslint-disable vue/multi-word-component-names -->
+<template>
+  <div>Hola</div>
+</template>
+
+<script>
+import { onMounted } from "vue";
+
+export default {
+  setup() {
+    onMounted(() => {
+      console.log("mounted");
+    });
+  },
+};
+</script>

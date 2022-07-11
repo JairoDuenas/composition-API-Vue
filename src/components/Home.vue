5aef5ab1983174f0fc15912107964ed27f56aad6
JairoDuenas <jairoduenas.ing@gmail.com>
2022-07-11 09:26:20 -0500

    Watch

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,18 +1,25 @@
 <!-- eslint-disable vue/multi-word-component-names -->
 <template>
   <div>{{ text }}</div>
-  <div>{{ obj }}</div>
+  <div>{{ obj.counter }}</div>
 </template>
 
 <script>
-import { ref, reactive } from "vue";
+import { ref, reactive, watch } from "vue";
 
 export default {
   setup() {
     const text = ref("Hola Vue");
     const obj = reactive({ counter: 0 });
 
-    setInterval(() => obj.counter++, 700);
+    setInterval(() => obj.counter++, 1000);
+
+    watch(
+      () => obj.counter,
+      (valor, anterior) => {
+        console.log(valor, anterior);
+      }
+    );
     return {
       text,
       obj,
